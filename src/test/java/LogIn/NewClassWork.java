607f52ddc6880b562329db51da8322459bf53dd4
lali-lilu <lalilabadze@gmail.com>
2022-10-28 23:15:35 +0400

    new homework

---
+++ src/test/java/LogIn/NewClassWork.java
@@ -0,0 +1,13 @@
+package LogIn;
+
+import ChromRunner.Chrom;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class NewClassWork extends Chrom{
+    @Test
+    public void FirstTests(){
+        
+    }
+}
